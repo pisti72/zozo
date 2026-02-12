dd31e4a780eb21fdfce3c23303f673fae37e9eb8
Istvan Szalontai <istvan.szalontai@catwalk-development.com>
2026-02-12 20:20:41 +0100

    gokart finished

--- python/infoalapvizsga/gokart.py
+++ python/infoalapvizsga/gokart.py
@@ -1,8 +1,46 @@
 f = open("gokart-adatok.txt")
 f.readline()
-pályák = list()
+NÉV = 0
+TELEPÜLÉS = 1
+HOSSZ = 2
+JEGYÁR = 3
+pályák = []
+
+def tizenötszázalékkal(alap):
+    return alap * 1.15
+
 for sor in f:
     rekord = sor.split(";")
     pályák.append(rekord)
 
-print(pályák[1][1])
+print("1. Feladat: Írd ki, hogy mennyi az összes pályát vizsgálva a pályák hossza!")
+összes_hossz = 0
+for pálya in pályák:
+    összes_hossz += int(pálya[HOSSZ])
+print(f"Az összes pálya hossza: {összes_hossz} m.")
+print("\n2. Feladat: Írd ki, hogy mi annak a pályának a neve,\nahol legolcsóbb a jegyár!")
+legolcsóbb_jegyár = int(pályák[0][JEGYÁR])
+legolcsóbb_név = pályák[0][NÉV]
+for pálya in pályák:
+    if legolcsóbb_jegyár > int(pálya[JEGYÁR]):
+        legolcsóbb_jegyár = int(pálya[JEGYÁR])
+        legolcsóbb_név = pálya[NÉV]
+print(f"A legolcsóbb jegy {legolcsóbb_név} pályán van, {legolcsóbb_jegyár} Ft.")
+print("\n3. Feladat: Az osztályon belül hozz létre egy tagfüggvényt áremelés néven")
+print("4. Feladat:")
+név = input("Írd be a pályának a nevét! ")
+emelt_ár = -1
+for pálya in pályák:
+    if pálya[NÉV].strip().lower() == név.strip().lower():
+        emelt_ár = tizenötszázalékkal(int(pálya[JEGYÁR]))
+if emelt_ár == -1:
+    print("Nem létezik ilyen pálya!")
+else:
+    print(f"Ennél a pályánál az áremeléssel {emelt_ár} Ft lenne a jegyár.")
+f = open("gokart-export.txt","w")
+pályák_budapestiek = ""
+for pálya in pályák:
+    if pálya[TELEPÜLÉS] == "Budapest":
+        pályák_budapestiek += pálya[NÉV] + "\n"
+f.write(pályák_budapestiek)
+f.close()
